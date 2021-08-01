6bd394471eb798ea22a769bc7adadede48cf339a
Parth Oberoi <htrap@gitopia.org>
2021-08-01 05:56:55 +0530

    git-server working with push pull clone fetch with git-go

---
+++ sample.go
@@ -0,0 +1,569 @@
+package main
+
+import (
+	contextB "context"
+	// "compress/gzip"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"path"
+	"path/filepath"
+	"strings"
+	"syscall"
+	// "time"
+	"github.com/go-git/go-billy/v5/osfs"
+	"github.com/go-git/go-git/v5/plumbing/cache"
+	"github.com/go-git/go-git/v5/plumbing/format/pktline"
+	"github.com/go-git/go-git/v5/plumbing/protocol/packp"
+	"github.com/go-git/go-git/v5/plumbing/storer"
+	"github.com/go-git/go-git/v5/plumbing/transport/server"
+	"regexp"
+	// "github.com/go-git/go-git/v5/storage/memory"
+	"github.com/go-git/go-git/v5/plumbing/transport"
+	"github.com/go-git/go-git/v5/storage/filesystem"
+)
+
+type mySimpleLoader struct{}
+
+func (mySimpleLoader) Load(_ *transport.Endpoint) (storer.Storer, error) {
+	fs := filesystem.NewStorage(osfs.New("/Users/parthoberoi/Code/gitopia-wd/gitsmart/repos/hat.git"), cache.NewObjectLRUDefault()) // this does not work.
+	return fs, fs.Init()
+	// return memory.NewStorage(), nil // this works.
+}
+
+func newWriteFlusher(w http.ResponseWriter) io.Writer {
+	return writeFlusher{w.(interface {
+		io.Writer
+		http.Flusher
+	})}
+}
+
+type writeFlusher struct {
+	wf interface {
+		io.Writer
+		http.Flusher
+	}
+}
+
+func (w writeFlusher) Write(p []byte) (int, error) {
+	defer w.wf.Flush()
+	return w.wf.Write(p)
+}
+
+type Credential struct {
+	Username string
+	Password string
+}
+
+func getCredential(req *http.Request) (Credential, error) {
+	cred := Credential{}
+
+	user, pass, ok := req.BasicAuth()
+	if !ok {
+		return cred, fmt.Errorf("authentication failed")
+	}
+
+	cred.Username = user
+	cred.Password = pass
+
+	return cred, nil
+}
+
+var reSlashDedup = regexp.MustCompile(`\/{2,}`)
+
+func fail500(w http.ResponseWriter, context string, err error) {
+	http.Error(w, "Internal server error", 500)
+	logError(context, err)
+}
+
+func logError(context string, err error) {
+	log.Printf("%s: %v\n", context, err)
+}
+
+func logInfo(context string, message string) {
+	log.Printf("%s: %s\n", context, message)
+}
+
+func cleanUpProcessGroup(cmd *exec.Cmd) {
+	if cmd == nil {
+		return
+	}
+
+	process := cmd.Process
+	if process != nil && process.Pid > 0 {
+		syscall.Kill(-process.Pid, syscall.SIGTERM)
+	}
+
+	go cmd.Wait()
+}
+
+func packLine(w io.Writer, s string) error {
+	_, err := fmt.Fprintf(w, "%04x%s", len(s)+4, s)
+	return err
+}
+
+func packFlush(w io.Writer) error {
+	_, err := fmt.Fprint(w, "0000")
+	return err
+}
+
+func subCommand(rpc string) string {
+	return strings.TrimPrefix(rpc, "git-")
+}
+
+// Parse out namespace and repository name from the path.
+// Examples:
+// repo -> "", "repo"
+// org/repo -> "org", "repo"
+// org/suborg/rpeo -> "org/suborg", "repo"
+func getNamespaceAndRepo(input string) (string, string) {
+	if input == "" || input == "/" {
+		return "", ""
+	}
+
+	// Remove duplicate slashes
+	input = reSlashDedup.ReplaceAllString(input, "/")
+
+	// Remove leading slash
+	if input[0] == '/' && input != "/" {
+		input = input[1:]
+	}
+
+	blocks := strings.Split(input, "/")
+	num := len(blocks)
+
+	if num < 2 {
+		return "", blocks[0]
+	}
+
+	return strings.Join(blocks[0:num-1], "/"), blocks[num-1]
+}
+
+type Config struct {
+	KeyDir     string       // Directory for server ssh keys. Only used in SSH strategy.
+	Dir        string       // Directory that contains repositories
+	GitPath    string       // Path to git binary
+	GitUser    string       // User for ssh connections
+	AutoCreate bool         // Automatically create repostories
+	AutoHooks  bool         // Automatically setup git hooks
+	Hooks      *HookScripts // Scripts for hooks/* directory
+	Auth       bool         // Require authentication
+}
+
+// HookScripts represents all repository server-size git hooks
+type HookScripts struct {
+	PreReceive  string
+	Update      string
+	PostReceive string
+}
+
+// Configure hook scripts in the repo base directory
+func (c *HookScripts) setupInDir(path string) error {
+	basePath := filepath.Join(path, "hooks")
+	scripts := map[string]string{
+		"pre-receive":  c.PreReceive,
+		"update":       c.Update,
+		"post-receive": c.PostReceive,
+	}
+
+	// Cleanup any existing hooks first
+	hookFiles, err := ioutil.ReadDir(basePath)
+	if err == nil {
+		for _, file := range hookFiles {
+			if err := os.Remove(filepath.Join(basePath, file.Name())); err != nil {
+				return err
+			}
+		}
+	}
+
+	// Write new hook files
+	for name, script := range scripts {
+		fullPath := filepath.Join(basePath, name)
+
+		// Dont create hook if there's no script content
+		if script == "" {
+			continue
+		}
+
+		if err := ioutil.WriteFile(fullPath, []byte(script), 0755); err != nil {
+			logError("hook-update", err)
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (c *Config) KeyPath() string {
+	return filepath.Join(c.KeyDir, "gitkit.rsa")
+}
+
+func (c *Config) Setup() error {
+	if _, err := os.Stat(c.Dir); err != nil {
+		if err = os.Mkdir(c.Dir, 0755); err != nil {
+			return err
+		}
+	}
+
+	if c.AutoHooks == true {
+		return c.setupHooks()
+	}
+
+	return nil
+}
+
+func (c *Config) setupHooks() error {
+	files, err := ioutil.ReadDir(c.Dir)
+	if err != nil {
+		return err
+	}
+
+	for _, file := range files {
+		if !file.IsDir() {
+			continue
+		}
+
+		path := filepath.Join(c.Dir, file.Name())
+
+		if err := c.Hooks.setupInDir(path); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+type service struct {
+	method  string
+	suffix  string
+	handler func(string, http.ResponseWriter, *Request)
+	rpc     string
+}
+
+type Server struct {
+	config   Config
+	services []service
+	AuthFunc func(Credential, *Request) (bool, error)
+}
+
+type Request struct {
+	*http.Request
+	RepoName string
+	RepoPath string
+}
+
+func New(cfg Config) *Server {
+	s := Server{config: cfg}
+	s.services = []service{
+		{"GET", "/info/refs", s.getInfoRefs, ""},
+		{"POST", "/git-upload-pack", s.postRPC, "git-upload-pack"},
+		{"POST", "/git-receive-pack", s.postRPC, "git-receive-pack"},
+	}
+
+	// Use PATH if full path is not specified
+	if s.config.GitPath == "" {
+		s.config.GitPath = "git"
+	}
+
+	return &s
+}
+
+// findService returns a matching git subservice and parsed repository name
+func (s *Server) findService(req *http.Request) (*service, string) {
+	for _, svc := range s.services {
+		if svc.method == req.Method && strings.HasSuffix(req.URL.Path, svc.suffix) {
+			path := strings.Replace(req.URL.Path, svc.suffix, "", 1)
+			return &svc, path
+		}
+	}
+	return nil, ""
+}
+
+func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	logInfo("request", r.Method+" "+r.Host+r.URL.String())
+
+	// Find the git subservice to handle the request
+	svc, repoUrlPath := s.findService(r)
+	if svc == nil {
+		http.Error(w, "Forbidden", http.StatusForbidden)
+		return
+	}
+
+	// Determine namespace and repo name from request path
+	repoNamespace, repoName := getNamespaceAndRepo(repoUrlPath)
+	if repoName == "" {
+		logError("auth", fmt.Errorf("no repo name provided"))
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	req := &Request{
+		Request:  r,
+		RepoName: path.Join(repoNamespace, repoName),
+		RepoPath: path.Join(s.config.Dir, repoNamespace, repoName),
+	}
+
+	if s.config.Auth {
+		if s.AuthFunc == nil {
+			logError("auth", fmt.Errorf("no auth backend provided"))
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+
+		authHeader := r.Header.Get("Authorization")
+		if authHeader == "" {
+			w.Header()["WWW-Authenticate"] = []string{`Basic realm=""`}
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+
+		cred, err := getCredential(r)
+		if err != nil {
+			logError("auth", err)
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+
+		allow, err := s.AuthFunc(cred, req)
+		if !allow || err != nil {
+			if err != nil {
+				logError("auth", err)
+			}
+
+			logError("auth", fmt.Errorf("rejected user %s", cred.Username))
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+	}
+
+	if !repoExists(req.RepoPath) && s.config.AutoCreate == true {
+		err := initRepo(req.RepoName, &s.config)
+		if err != nil {
+			logError("repo-init", err)
+		}
+	}
+
+	if !repoExists(req.RepoPath) {
+		logError("repo-init", fmt.Errorf("%s does not exist", req.RepoPath))
+		http.NotFound(w, r)
+		return
+	}
+
+	svc.handler(svc.rpc, w, req)
+}
+
+func (s *Server) getInfoRefs(_ string, w http.ResponseWriter, r *Request) {
+	context := "get-info-refs"
+	rpc := r.URL.Query().Get("service")
+	defer r.Body.Close()
+
+	w.Header().Add("Content-Type", fmt.Sprintf("application/x-%s-advertisement", rpc))
+	w.Header().Add("Cache-Control", "no-cache")
+	w.WriteHeader(200)
+
+	if !(rpc == "git-upload-pack" || rpc == "git-receive-pack") {
+		http.Error(w, "Not Found", 404)
+		return
+	}
+
+	session := server.NewServer(mySimpleLoader{})
+
+
+	switch rpc {
+	case "git-receive-pack":
+		ts, err := session.NewReceivePackSession(&transport.Endpoint{}, nil)
+		if err != nil {
+			logError(context, err)
+		}
+		defer ts.Close()
+		adv, err := ts.AdvertisedReferences()
+		if err != nil {
+			logError(context, err)
+		}
+
+		// Enable smart protocol for http
+		enc := pktline.NewEncoder(w)
+		enc.Encode([]byte(fmt.Sprintf("# service=%s\n", rpc)))
+		enc.Encode(nil)
+
+		if err := adv.Encode(w); err != nil {
+			logError(context, err)
+			return
+		}
+
+	case "git-upload-pack":
+		ts, err := session.NewReceivePackSession(&transport.Endpoint{}, nil)
+		if err != nil {
+			logError(context, err)
+		}
+		defer ts.Close()
+		adv, err := ts.AdvertisedReferences()
+		if err != nil {
+			logError(context, err)
+		}
+
+		// Enable smart protocol for http
+		enc := pktline.NewEncoder(w)
+		enc.Encode([]byte(fmt.Sprintf("# service=%s\n", rpc)))
+		enc.Encode(nil)
+
+		if err := adv.Encode(w); err != nil {
+			logError(context, err)
+			return
+		}
+	}
+
+
+	// cmd, pipe := gitCommand(s.config.GitPath, subCommand(rpc), "--stateless-rpc", "--advertise-refs", r.RepoPath)
+	// if err := cmd.Start(); err != nil {
+	// 	fail500(w, context, err)
+	// 	return
+	// }
+	// defer cleanUpProcessGroup(cmd)
+
+	// if err := packLine(w, fmt.Sprintf("# service=%s\n", rpc)); err != nil {
+	// 	logError(context, err)
+	// 	return
+	// }
+
+	// if err := packFlush(w); err != nil {
+	// 	logError(context, err)
+	// 	return
+	// }
+
+}
+
+// if err := cmd.Wait(); err != nil {
+// 	logError(context, err)
+// 	return
+// }
+
+func (s *Server) postRPC(rpc string, w http.ResponseWriter, r *Request) {
+	// context := "post-rpc"
+	// body := r.Body
+	defer r.Body.Close()
+
+	// if r.Header.Get("Content-Encoding") == "gzip" {
+	// 	var err error
+	// 	_, err := gzip.NewReader(r.Body)
+	// 	if err != nil {
+	// 		fail500(w, context, err)
+	// 		return
+	// 	}
+	// }
+	session := server.NewServer(mySimpleLoader{})
+
+	w.Header().Add("Content-Type", fmt.Sprintf("application/x-%s-result", rpc))
+	w.Header().Add("Cache-Control", "no-cache")
+	w.WriteHeader(200)
+
+	switch rpc {
+	case "git-receive-pack":
+
+		ts, err := session.NewReceivePackSession(&transport.Endpoint{}, nil)
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer ts.Close()
+
+		req := packp.NewReferenceUpdateRequest()
+		if err := req.Decode(r.Body); err != nil {
+			return
+		}
+
+		status, err := ts.ReceivePack(contextB.TODO(), req)
+		if status != nil {
+			if err := status.Encode(w); err != nil {
+				fmt.Println(err)
+			}
+		}
+
+		if err != nil {
+			fmt.Println(err)
+		}
+
+	case "git-upload-pack":
+		ts, err := session.NewUploadPackSession(&transport.Endpoint{}, nil)
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer ts.Close()
+
+		req := packp.NewUploadPackRequest()
+		if err := req.Decode(r.Body); err != nil {
+			return
+		}
+
+		status, err := ts.UploadPack(contextB.TODO(), req)
+		if status != nil {
+			if err := status.Encode(w); err != nil {
+				fmt.Println(err)
+			}
+		}
+
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+
+}
+
+func (s *Server) Setup() error {
+	return s.config.Setup()
+}
+
+func initRepo(name string, config *Config) error {
+	fullPath := path.Join(config.Dir, name)
+
+	if err := exec.Command(config.GitPath, "init", "--bare", fullPath).Run(); err != nil {
+		return err
+	}
+
+	if config.AutoHooks && config.Hooks != nil {
+		return config.Hooks.setupInDir(fullPath)
+	}
+
+	return nil
+}
+
+func repoExists(p string) bool {
+	_, err := os.Stat(path.Join(p, "objects"))
+	return err == nil
+}
+
+func gitCommand(name string, args ...string) (*exec.Cmd, io.Reader) {
+	cmd := exec.Command(name, args...)
+	cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
+	cmd.Env = os.Environ()
+
+	r, _ := cmd.StdoutPipe()
+	cmd.Stderr = cmd.Stdout
+
+	return cmd, r
+}
+
+func main() {
+
+	// Configure git service
+	service := New(Config{
+		Dir:        "/Users/parthoberoi/Code/gitopia-wd/gitsmart/repos",
+		AutoCreate: true,
+	})
+
+	// Configure git server. Will create git repos path if it does not exist.
+	// If hooks are set, it will also update all repos with new version of hook scripts.
+	if err := service.Setup(); err != nil {
+		log.Fatal(err)
+	}
+
+	http.Handle("/", service)
+
+	// Start HTTP server
+	if err := http.ListenAndServe(":5000", nil); err != nil {
+		log.Fatal(err)
+	}
+}
