f12c48ceb37f49180f2072f60de5143e415b457e
faza <faza@gitopia.org>
2021-09-18 15:31:35 +0530

    Give sha and size in response for `/upload`

--- handler.go
+++ handler.go
@@ -3,6 +3,7 @@ package main
 import (
 	"crypto/sha256"
 	"encoding/hex"
+	"encoding/json"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -13,6 +14,11 @@ import (
 	"github.com/spf13/viper"
 )
 
+type uploadAttachmentResponse struct {
+	Sha  string `json:"sha"`
+	Size int64  `json:"size"`
+}
+
 func uploadAttachmentHandler(w http.ResponseWriter, r *http.Request) {
 
 	err := r.ParseMultipartForm(32 << 20)
@@ -21,7 +27,7 @@ func uploadAttachmentHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	file, _, err := r.FormFile("file")
+	file, handler, err := r.FormFile("file")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
@@ -40,14 +46,24 @@ func uploadAttachmentHandler(w http.ResponseWriter, r *http.Request) {
 	_, err = io.Copy(io.MultiWriter(sha, tmpFile), file)
 
 	attachmentDir := viper.GetString("attachment_dir")
-	fileName := fmt.Sprintf("%s/%s", attachmentDir, hex.EncodeToString(sha.Sum(nil)))
-	localFile, err := os.Create(fileName)
+	shaString := hex.EncodeToString(sha.Sum(nil))
+	filePath := fmt.Sprintf("%s/%s", attachmentDir, shaString)
+	localFile, err := os.Create(filePath)
 	defer localFile.Close()
 	_, err = io.Copy(localFile, tmpFile)
 	if err != nil {
 		logError("cannot copy from temp file to attachment dir", err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	resp := uploadAttachmentResponse{
+		Sha:  shaString,
+		Size: handler.Size,
+	}
+
+	json.NewEncoder(w).Encode(resp)
 
 	return
 }
