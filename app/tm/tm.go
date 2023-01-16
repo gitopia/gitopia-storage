2d6810e60e5e49323207875cca9782934a252df9
janani anbarasan <janani.anbarasan@gmail.com>
2023-01-15 23:52:37 -0600

    handle ws connection failure

--- app/tm/tm.go
+++ app/tm/tm.go
@@ -7,11 +7,13 @@ import (
 	"github.com/gitopia/gitopia-go/logger"
 	"github.com/pkg/errors"
 	"github.com/tendermint/tendermint/rpc/jsonrpc/client"
+	"github.com/tendermint/tendermint/rpc/jsonrpc/types"
 )
 
 const (
 	TM_WS_ENDPOINT    = "/websocket"
-	TM_WS_PING_PERIOD = 15 * time.Second
+	TM_WS_PING_PERIOD = 10 * time.Second
+	TM_WS_MAX_RECONNECT = 3
 )
 
 type Client struct {
@@ -22,7 +24,8 @@ type evenHandlerFunc func(context.Context, []byte) error
 
 func NewClient(addr string) (*Client, error) {
 	wsc, err := client.NewWS(addr, TM_WS_ENDPOINT,
-		client.PingPeriod(TM_WS_PING_PERIOD))
+		client.PingPeriod(TM_WS_PING_PERIOD),
+		client.MaxReconnectAttempts(TM_WS_MAX_RECONNECT))
 	if err != nil {
 		return nil, errors.Wrap(err, "error creating ws client")
 	}
@@ -49,11 +52,17 @@ func (c Client) Subscribe(ctx context.Context, q string, h evenHandlerFunc) (<-c
 			return
 		}
 		for {
-			event := <-c.c.ResponsesCh
-			if event.Error != nil {
-				e <- errors.Wrap(err, "error reading from ws")
+			var event types.RPCResponse
+			select {
+			case event = <- c.c.ResponsesCh:
+			case <- c.c.Quit():
+				e <- errors.New("ws conn closed")
 				return
 			}
+			if event.Error != nil {
+				e <- errors.Wrap(event.Error, "error reading from ws")
+				return
+			} 
 
 			jsonBuf, err := event.Result.MarshalJSON()
 			if err != nil {
--- go.mod
+++ go.mod
@@ -12,7 +12,6 @@ require (
 	github.com/libgit2/git2go/v33 v33.0.4
 	github.com/pkg/errors v0.9.1
 	github.com/rs/cors v1.8.2
-	github.com/sirupsen/logrus v1.9.0
 	github.com/spf13/cobra v1.5.0
 	github.com/spf13/viper v1.13.0
 	github.com/tendermint/tendermint v0.34.22
@@ -131,8 +130,8 @@ require (
 	github.com/prometheus/procfs v0.7.3 // indirect
 	github.com/rakyll/statik v0.1.7 // indirect
 	github.com/rcrowley/go-metrics v0.0.0-20201227073835-cf1acfcdf475 // indirect
-	github.com/rogpeppe/go-internal v1.8.1 // indirect
 	github.com/sasha-s/go-deadlock v0.3.1 // indirect
+	github.com/sirupsen/logrus v1.9.0 // indirect
 	github.com/spf13/afero v1.8.2 // indirect
 	github.com/spf13/cast v1.5.0 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
