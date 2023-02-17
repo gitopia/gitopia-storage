f11b111af3711fcbc50c26ac7ac70ea2b3ce1977
janani anbarasan <janani.anbarasan@gmail.com>
2023-02-16 22:58:10 -0600

    do not use std err for logging

--- hooks/gitopia-pre-receive/main.go
+++ hooks/gitopia-pre-receive/main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-  "log"
-  "os"
-  //"fmt"
+	"fmt"
+	"os"
 
-  "github.com/sosedoff/gitkit"
+	"github.com/sosedoff/gitkit"
+	//"fmt"
 )
 
 // HookInfo contains information about branch, before and after revisions.
@@ -17,16 +17,16 @@ func receive(hook *gitkit.HookInfo, tmpPath string) error {
   // log.Println("Old revision:", hook.OldRev)
   // log.Println("New revision:", hook.NewRev)
 
-  // // Check if push is non fast-forward (force)
-  // force, err := gitkit.IsForcePush(hook)
-  // if err != nil {
-  //   return err
-  // }
+  // Check if push is non fast-forward (force)
+  force, err := gitkit.IsForcePush(hook)
+  if err != nil {
+    return err
+  }
 
-  // // Reject force push
-  // if force {
-  //   return fmt.Errorf("non fast-forward pushed are not allowed")
-  // }
+  // Reject force push
+  if force {
+    return fmt.Errorf("non fast-forward pushed are not allowed")
+  }
 
   // // Check if branch is being deleted
   // if hook.Action == gitkit.BranchDeleteAction {
@@ -45,8 +45,11 @@ func receive(hook *gitkit.HookInfo, tmpPath string) error {
 }
 
 func main() {
-  os.Exit(1)
-  return
+  // f, err  := os.OpenFile("/tmp/gitkit/app.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 777)
+  // if err != nil {
+  //   panic(err)
+  // }
+  // log.SetOutput(f)
   receiver := gitkit.Receiver{
     MasterOnly:  false,         // if set to true, only pushes to master branch will be allowed
     TmpDir:      "/tmp/gitkit", // directory for temporary git checkouts
@@ -55,7 +58,7 @@ func main() {
 
   // Git hook data is provided via STDIN
   if err := receiver.Handle(os.Stdin); err != nil {
-    log.Println("Error:", err)
+    // log.Println("Error:", err)
     os.Exit(1) // terminating with non-zero status will cancel push
   }
   // os.Exit(0)
