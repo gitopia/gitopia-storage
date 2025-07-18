e8436b656d6fc059d90423b899ae1d22e29764c1
faza <faza@gitopia.org>
2025-07-18 17:11:43 +0530

    feat(build): Exclude profiling tools in production build
    The profiling functions (startPprofServer and startMemoryMonitor) have been moved into a debug.go file, which is excluded from compilation when the prod build tag is used. A corresponding prod.go file provides no-op stubs for production builds.

--- cmd/gitopia-storaged/start.go
+++ cmd/gitopia-storaged/start.go
@@ -106,43 +106,7 @@ func start(cmd *cobra.Command, args []string) error {
 	return g.Wait()
 }
 
-func startPprofServer(ctx context.Context, port int) error {
-	addr := fmt.Sprintf("localhost:%d", port)
-	logrus.Infof("starting pprof server on http://%s/debug/pprof/", addr)
 
-	server := &http.Server{Addr: addr, Handler: nil} // Use default mux
-
-	go func() {
-		<-ctx.Done()
-		shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-		defer cancel()
-		server.Shutdown(shutdownCtx)
-	}()
-
-	if err := server.ListenAndServe(); err != http.ErrServerClosed {
-		return errors.Wrap(err, "pprof server error")
-	}
-	logrus.Info("pprof server stopped")
-	return nil
-}
-
-func startMemoryMonitor(ctx context.Context) {
-	ticker := time.NewTicker(5 * time.Minute)
-	defer ticker.Stop()
-
-	for {
-		select {
-		case <-ticker.C:
-			var m runtime.MemStats
-			runtime.ReadMemStats(&m)
-			logrus.Infof("Memory Usage: Alloc = %v MiB, TotalAlloc = %v MiB, Sys = %v MiB, NumGC = %v",
-				m.Alloc/1024/1024, m.TotalAlloc/1024/1024, m.Sys/1024/1024, m.NumGC)
-		case <-ctx.Done():
-			logrus.Info("memory monitor stopped")
-			return
-		}
-	}
-}
 
 func startWebServer(ctx context.Context, cmd *cobra.Command, batchTxManager *app.BatchTxManager) error {
 	server, err := setupWebServer(cmd, batchTxManager)
